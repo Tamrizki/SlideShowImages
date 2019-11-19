3e01bc884bfc721cd863b12c79e4a08b8dbaeafd
tamrizki <rizkitam77@gmail.com>
2019-11-19 19:16:19 +0700

    set slide Show Images

--- app/src/main/java/tam/ttr/slideshowimages/MainActivity.java
+++ app/src/main/java/tam/ttr/slideshowimages/MainActivity.java
@@ -3,12 +3,44 @@ package tam.ttr.slideshowimages;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Deklarasi ViewFlipper [1]
+    private ViewFlipper flipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Inisialisasi ViewFlipper yang telah di deklarasikan di sebelumnya [2]
+        flipper = findViewById(R.id.flipper);
+
+//        Buat Array untuk menampung gambar yang akan ditampilkan [3]
+        int images[] = {R.drawable.img1, R.drawable.img2, R.drawable.img3};
+
+//      memanggil methode yang telah dibuat dengan melakukan perulangan untuk mengganti gambar [5]
+        for (int i =0; i<images.length; i++){
+            SlideImages(images[i]);
+        }
+    }
+
+    //    Buat methode untuk mengatur ViewFlipper [4]
+    public  void  SlideImages(int images){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(images);
+
+        flipper.addView(imageView);
+
+//        Atur durasi perpindahan gambar
+        flipper.setFlipInterval(4000);
+        flipper.setAutoStart(true);
+
+        flipper.setInAnimation(this,android.R.anim.slide_in_left);
+        flipper.setOutAnimation(this,android.R.anim.slide_out_right);
+
     }
 }
